55796c80ba6e93f8624aead6ffb5c93347a50d5c
Wendel <wendel.isaac12@gmail.com>
2024-04-17 01:36:06 -0300

    feat: animação para a escolha do computador

--- src/script/script.js
+++ src/script/script.js
@@ -57,16 +57,35 @@ $("document").ready(()=>{
 
                 var randomNumber = 1 + Math.floor(Math.random() * 3);
                 var houseChosen = ""
+
                 if(randomNumber == 1){
-                    $("#house-option").html('<div id="option-selected" class="paper"><img src="src/images/icon-paper.svg" alt=""></div>')     
                     houseChosen = "paper"
                 }else if(randomNumber == 2){
-                    $("#house-option").html('<div id="option-selected" class="scissors"><img src="src/images/icon-scissors.svg" alt=""></div>')
                     houseChosen = "scissors"
-                }else if(randomNumber == 3){
-                    $("#house-option").html('<div id="option-selected" class="rock"><img src="src/images/icon-rock.svg" alt=""></div>')
+                }else if(randomNumber == 3){                    
                     houseChosen = "rock"
                 }
+                
+                var showChosenHouse = setInterval(() => {
+                    if(houseChosen == "paper"){
+                        $("#house-option").html('<div class="op"><div id="option-selected" class="paper"><img src="src/images/icon-paper.svg" alt=""></div></div>')     
+                    }else if(houseChosen == "scissors"){
+                        $("#house-option").html('<div class="op"><div id="option-selected" class="scissors"><img src="src/images/icon-scissors.svg" alt=""></div></div>')
+                    }else if(houseChosen == "rock"){
+                        $("#house-option").html('<div class="op"><div id="option-selected" class="rock"><img src="src/images/icon-rock.svg" alt=""></div></div>')
+                    }
+                    $(".op").fadeIn()
+                    $(".op").addClass("scale-animation");
+
+                    setTimeout(() => {
+                        $(".op").removeClass("bounce-animation");
+                        $(".op").addClass("scale-back-animation");
+                    }, 500);
+                    clearInterval(showChosenHouse)
+                }, 800);
+
+                
+
 
                 console.log(gameRules(escolhido, houseChosen))
 
